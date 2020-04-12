f2c8bf4bab1ca6fced51f5c22da7545980b5a03c
adrientremblay <adrien@adrientremblay.com>
2020-04-11 21:30:32 -0400

    function to convert from polar to rectangular

--- cal.c
+++ cal.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-#include <complex.h>
+#define PI 3.14159265
 
+// Polar form complex number
 struct polar {
   double mag; // magnitude component
   double ang; // angle component
 };
 
+// Rectangular form complex number
 struct rect {
   double re; // real component 
   double im; // imaginary component 
 };
 
-/* double polar_to_rect(double rect_num) { */
+struct rect polar_to_rect(struct polar polar_num) {
+  double re = polar_num.mag * cos(polar_num.ang);
+  double im = polar_num.mag * sin(polar_num.ang);
   
-
-/*   return */ 
-/* } */
+  struct rect rect_num = {re, im};
+  return rect_num;
+}
 
 int main() {
+  struct polar test_polar = {10, PI/3};
+  struct rect converted = polar_to_rect(test_polar);
+  printf("%f %f", converted.re, converted.im); 
 
   return 0;
 }
