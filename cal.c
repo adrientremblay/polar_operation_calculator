156e461473fea883e37d75421fcbe6bf2337e540
adrientremblay <adrien@adrientremblay.com>
2020-04-11 20:51:22 -0400

    Create rectangular and polar form structs

---
+++ cal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+
+struct polar {
+  double mag; // magnitude component
+  double ang; // angle component
+};
+
+struct rect {
+  double re; // real component 
+  double im; // imaginary component 
+};
+
+/* double polar_to_rect(double rect_num) { */
+  
+
+/*   return */ 
+/* } */
+
+int main() {
+
+  return 0;
+}
